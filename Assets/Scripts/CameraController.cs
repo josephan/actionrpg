e189728a10b88fe8f3532935a573d6b3850c9ece
Joseph An <an.joseph891@gmail.com>
2017-01-22 00:53:21 -0500

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var followTargetPos = followTarget.transform.position;
+		targetPos = new Vector3(followTargetPos.x, followTargetPos.y, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
